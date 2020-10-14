d32ed415d086b87bc577d93398c0aff5b93cd96f
ismailarilik <arilik.ismail@gmail.com>
2020-10-14 12:37:28 +0300

    Remove unnecessary test

--- spec/rock_paper_scissors_spec.rb
+++ spec/rock_paper_scissors_spec.rb
@@ -4,9 +4,4 @@ RSpec.describe RockPaperScissors do
   it 'has a version number' do
     expect(RockPaperScissors::VERSION).not_to be nil
   end
-
-  it 'does something useful' do
-    value = false
-    expect(value).to eq(true)
-  end
 end
